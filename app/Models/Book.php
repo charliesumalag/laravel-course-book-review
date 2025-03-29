0dc329f280d6ce1909c29fbe5410b9a7c293ade1
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-03-29 15:18:42 +0800

    Local Querries

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -9,10 +10,20 @@ class Book extends Model
 {
     use HasFactory;
 
+    // protected $fillable = ['review', 'rating'];
+
     public function reviews()
     {
         return $this->hasMany(Review::class);
     }
 
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
 
+    public function scopeAuthor(Builder $query, string $author): Builder
+    {
+        return $query->where('Author', 'LIKE', '%' . $author . '%');
+    }
 }
